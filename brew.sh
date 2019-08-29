ed495203a4348fe1351e982952fa320282f78cbe
Grégoire <gregoire.david@ymail.com>
2019-08-29 17:14:44 +0200

    add a few files

---
+++ brew.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Install homebrew if it is not installed
+which brew 1>&/dev/null
+if [ ! "$?" -eq 0 ] ; then
+	echo "Homebrew not installed. Attempting to install Homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	if [ ! "$?" -eq 0 ] ; then
+		echo "Something went wrong. Exiting..." && exit 1
+	fi
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Core Utils
+brew install coreutils
+
+
+# ---------------------------------------------
+# Programming Languages and Frameworks
+# ---------------------------------------------
+
+# NodeJS 
+brew install node
+
+# Python 3
+brew install python
+
+# ---------------------------------------------
+# Useful tools
+# ---------------------------------------------
+
+# Make requests with awesome response formatting
+brew install httpie
+
+# Show directory structure with excellent formatting
+brew install tree
+
+# tmux :'D 
+brew install tmux
+
+# Remove outdated versions from the cellar
+brew cleanup
--- install.sh
+++ install.sh
@@ -1,3 +1,33 @@
-ln -sv “~/.dotfiles/.bash_profile”
-ln -sv “~/.dotfiles/.alias”
-ln -sv “~/.dotfiles/git/.gitconfig”
+link () {
+	echo "This utility will symlink the files in this repo to the home directory"
+	echo "Proceed? (y/n)"
+	read resp
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		for file in $( ls -A | grep -vE '\.git$|.*\.sh$|.*\.md$' ) ; do
+			ln -sv "$PWD/$file" "$HOME"
+		done
+		echo "Symlinking complete"
+	else
+		echo "Symlinking cancelled by user"
+		return 1
+	fi
+}
+
+install_tools () {
+	if [ $( echo "$OSTYPE" | grep 'darwin' ) ] ; then
+		echo "This utility will install useful utilities using Homebrew"
+		echo "Proceed? (y/n)"
+		read resp
+		if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+			echo "Installing useful stuff using brew. This may take a while..."
+			sh brew.sh
+		else
+			echo "Brew installation cancelled by user"
+		fi
+	else
+		echo "Skipping installations using Homebrew because MacOS was not detected..."
+	fi
+}
+
+link
+install_tools
